1c950f786479f8b3cbe69f497ba74ddaf6b5f42e
Guanghong Zuo <ghzuo@fudan.edu.cn>
2015-09-14 18:03:12 +0800

    rearrange

--- src/02/bool.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-
-int main(){
-    _Bool y;
-    int x = sizeof(y);
-    printf("%i\n", x);
-}
--- src/02/bool.cpp
+++
@@ -1,7 +0,0 @@
-#include <iostream>
-using namespace std;
-
-int main(){
-    bool b;
-    cout << sizeof(b) << endl;
-}
