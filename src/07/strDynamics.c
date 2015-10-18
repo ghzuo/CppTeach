f0232c6481005938471e5e3b736ea2d4311f52af
G.H. Zuo <ghzuo@fudan.edu.cn>
2015-10-18 22:36:22 +0800

    update

--- src/07/strDynamics.c
+++ src/07/strDynamics.c
@@ -2,18 +2,41 @@
 #include <string.h>
 #include <stdlib.h>
 
-void strAssign(char** dest, char* src){
-    *dest = malloc(strlen(src)+1);
-    strcpy(*dest, src);
+typedef struct string{
+    char* pstr;
+} String;
+
+void strByCstr(String* str, char* v){
+    if((*str).pstr != NULL)
+        free((*str).pstr);
+    (*str).pstr = malloc(strlen(v)+1);
+    strcpy((*str).pstr, v);
 }
 
-void streq(char** s){
-    char ptr[] = "looooooooooooooooong";
-    strAssign(s, ptr);
+void strByChar(String* str, char v){
+    if((*str).pstr != NULL)
+        free((*str).pstr);
+    (*str).pstr = malloc(2);
+    (*str).pstr[0] = v;
+    (*str).pstr[1] = '\0';
+}
+
+void strByStr(String* str, String v){
+    if((*str).pstr != NULL)
+        free((*str).pstr);
+    (*str).pstr = malloc(strlen(v.pstr)+1);
+    strcpy((*str).pstr, v.pstr);
+}
+
+void streq(String* s){
+    String ptr;
+    strByCstr(&ptr, "XXXXX");
+    strByStr(s, ptr);
 }
 
 int main(){
-    char* str;
+    String str;  // = between String will be very dangerous ...
     streq(&str);
-    printf("%s\n", str);
+    printf("%s\n", str.pstr);
 }
+
