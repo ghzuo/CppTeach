f7dcaad5f474daa881941f92c648b0759acca229
G.H. Zuo <ghzuo@fudan.edu.cn>
2015-10-23 22:06:41 +0800

    for string of c

--- src/06/strDynamics.c
+++ src/06/strDynamics.c
@@ -2,49 +2,58 @@
 #include <string.h>
 #include <stdlib.h>
 
+// Defination of the string
 typedef struct string{
     char* pstr;
 } String;
 
+// delete string
+void strDelete(String* str){
+    free((*str).pstr);
+    (*str).pstr = NULL;
+}
+
+// Intial string
+void strInitial(String* str){
+    (*str).pstr = NULL;
+}
+
+// assign by c type string
 void strByCstr(String* str, char* v){
-    if((*str).pstr != NULL){
-        free((*str).pstr);
-        (*str).pstr = NULL;
-    }
+    if((*str).pstr != NULL)
+        strDelete(str);
     
     (*str).pstr = (char*) malloc(strlen(v)+1);
     strcpy((*str).pstr, v);
 }
 
+// assign by char
 void strByChar(String* str, char v){
-    if((*str).pstr != NULL){
-        free((*str).pstr);
-        (*str).pstr = NULL;
-    }
-        
+    if((*str).pstr != NULL)
+        strDelete(str);
+    
     (*str).pstr = (char*) malloc(2);
     (*str).pstr[0] = v;
     (*str).pstr[1] = '\0';
 }
 
+// assign by string
 void strByStr(String* str, String v){
-    if((*str).pstr != NULL){
-        free((*str).pstr);
-        (*str).pstr = NULL;
-    }
-    
+    if((*str).pstr != NULL)
+        strDelete(str);
+
     (*str).pstr = (char*) malloc(strlen(v.pstr)+1);
     strcpy((*str).pstr, v.pstr);
 }
 
 void streq(String* s){
     char v[] = "Very Very Very Loooooooooooooooooooooooog";
-    String ptr = {NULL};
     strByCstr(s, v);
 }
 
 int main(){
-    String str = {NULL};
+    String str;
+    strInitial(&str);
     
     streq(&str);
     printf("%s\n", str.pstr);
