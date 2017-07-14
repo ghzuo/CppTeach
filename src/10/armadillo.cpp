dfd299574f61ea37ba119fb6c27704c54cd320d3
Guanghong Zuo <ghzuo@fudan.edu.cn>
2017-07-14 09:11:29 +0800

    update the eigen Matrix

--- src/10/armadillo.cpp
+++ src/10/armadillo.cpp
@@ -7,18 +7,20 @@ using namespace arma;
 
 int main(){
 
-    mat A("1 2; 2 3");
+    mat A("0.6606, 0.1751, 0.058,  0.0314, 0.0459, 0.029; 0.1826, 0.7138, 0.02,   0.0506, 0.0271, 0.0059; 0.0747, 0.0317, 0.7783, 0.0045, 0.0339, 0.0769; 0.0394, 0.0627, 0.0072, 0.6989, 0.0269, 0.1649; 0.2412, 0.1294, 0.0471, 0.0941, 0.4412, 0.0471; 0.0109, 0.0019, 0.0114, 0.0395, 0.0019, 0.9344;");
+
+    A = A.t();
     cout << "Here is the matrix A:\n" << A << endl;
 
     // get the Eigen
-    vec eigval;
-    mat eigvec;
-    eig_sym(eigval, eigvec, A);
+    cx_vec eigval;
+    cx_mat eigvec;
+    eig_gen(eigval, eigvec, A);
 
     cout << "The eigenvalues of A are:\n"
-        << eigval << endl;
+         << eigval << endl;
     cout << "Here's a matrix whose columns are eigenvectors of A \n"
-        << "corresponding to these eigenvalues:\n"
-        << eigvec << endl;
+         << "corresponding to these eigenvalues:\n"
+         << eigvec << endl;
 }
 
--- src/10/eigen.cpp
+++ src/10/eigen.cpp
@@ -3,22 +3,31 @@
 using namespace std;
 using namespace Eigen;
 
+typedef Matrix<float,6,6> Matrix6f;
+
 int main(){
 
-   Matrix2f A;
-   A << 1, 2, 2, 3;
-   cout << "Here is the matrix A:\n" << A << endl;
+    Matrix6f A;
+    A <<0.6606, 0.1751, 0.0580, 0.0314, 0.0459, 0.0290,
+        0.1826, 0.7138, 0.0200, 0.0506, 0.0271, 0.0059,
+        0.0747, 0.0317, 0.7783, 0.0045, 0.0339, 0.0769,
+        0.0394, 0.0627, 0.0072, 0.6989, 0.0269, 0.1649,
+        0.2412, 0.1294, 0.0471, 0.0941, 0.4412, 0.0470,
+        0.0109, 0.0019, 0.0114, 0.0395, 0.0019, 0.9344;
+    A.transposeInPlace();
+   
+    cout << "Here is the matrix A:\n" << A << endl;
 
-   // get the eigen
-   SelfAdjointEigenSolver<Matrix2f> eigensolver(A);
-   // EigenSolver<Matrix2f> eigensolver(A);
+    // get the eigen
+    EigenSolver<Matrix6f> eigensolver(A);
+    // EigenSolver<Matrix2f> eigensolver(A);
 
-   if (eigensolver.info() != Success) abort();
+    if (eigensolver.info() != Success) abort();
 
-   cout << "The eigenvalues of A are:\n"
-        << eigensolver.eigenvalues() << endl;
-   cout << "Here's a matrix whose columns are eigenvectors of A \n"
-        << "corresponding to these eigenvalues:\n"
-        << eigensolver.eigenvectors() << endl;
+    cout << "The eigenvalues of A are:\n"
+         << eigensolver.eigenvalues() << endl;
+    cout << "Here's a matrix whose columns are eigenvectors of A \n"
+         << "corresponding to these eigenvalues:\n"
+         << eigensolver.eigenvectors() << endl;
 }
 
