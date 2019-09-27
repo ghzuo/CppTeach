0b00914e6ecb086cd9b696af4a6b74284f27a268
Guangong Zuo <ghzuo@fudan.edu.cn>
2019-09-27 18:06:53 +0800

    add hello code

---
+++ src/demo/02/hello.c
@@ -0,0 +1,17 @@
+/*
+ * Copyright (c) 2019  T-Life Research Center, Fudan University, Shanghai, China.
+ * See the accompanying Manual for the contributors and the way to cite this work.
+ * Comments and suggestions welcome. Please contact
+ * Dr. Guanghong Zuo <ghzuo@fudan.edu.cn>
+ * 
+ * @Author: Dr. Guanghong Zuo
+ * @Date: 2019-09-27 14:18:26
+ * @Last Modified By: Dr. Guanghong Zuo
+ * @Last Modified Time: 2019-09-27 14:18:26
+ */
+
+#include <stdio.h>
+
+int main(){
+  printf("Hello World!\n");
+}
