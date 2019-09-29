d02b751b83393eae8eb99ab342f61abb892fb47d
Guangong Zuo <ghzuo@fudan.edu.cn>
2019-09-29 18:15:42 +0800

    update file format and tasks.josn

--- src/demo/03/args.c
+++ src/demo/03/args.c
@@ -1,28 +1,31 @@
-#include <stdio.h>
 #include <ctype.h>
+#include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
 
-main(int argc, char *argv[]){
-    int x;
-    int y = 1;
-    char *str="XXXX";
+main(int argc, char *argv[]) {
+  int x;
+  int y = 1;
+  char *str = "XXXX";
 
-    char ch;
-    while ((ch = getopt(argc, argv, "x:s:yh")) != -1){
-      switch (ch){
-      case 'x':
-	  x = atoi(optarg); break;
-      case 's': 
-	  str = optarg; break;
-      case 'y':
-	  y = 3; break;
-      case '?':
-	  printf("Wrong input arguments!\n"); 
-      }
+  char ch;
+  while ((ch = getopt(argc, argv, "x:s:yh")) != -1) {
+    switch (ch) {
+    case 'x':
+      x = atoi(optarg);
+      break;
+    case 's':
+      str = optarg;
+      break;
+    case 'y':
+      y = 3;
+      break;
+    case '?':
+      printf("Wrong input arguments!\n");
     }
+  }
 
-    printf("the x is: %d\n", x);
-    printf("the y is: %d\n", y);
-    printf("the str is: %s\n", str);
+  printf("the x is: %d\n", x);
+  printf("the y is: %d\n", y);
+  printf("the str is: %s\n", str);
 }
