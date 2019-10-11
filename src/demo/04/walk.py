3fa6a99afb8e6741453d6c98cfa3ad4fd9497ef1
Guanghong Zuo <ghzuo@fudan.edu.cn>
2019-10-11 10:24:56 +0800

    change python code for walk

--- src/demo/04/walk.py
+++ src/demo/04/walk.py
@@ -1,24 +1,36 @@
-#!/usr/bin/python
+#!/usr/bin/env python3
+# -*- coding:utf-8 -*-
+'''
+Copyright (c) 2019  T-Life Research Center, Fudan University, Shanghai, China.
+See the accompanying Manual for the contributors and the way to cite this work.
+Comments and suggestions welcome. Please contact
+Dr. Guanghong Zuo <ghzuo@fudan.edu.cn>
+
+@Author: Dr. Guanghong Zuo
+@Date: 2016-04-19 11:56:28
+@Last Modified By: Dr. Guanghong Zuo
+@Last Modified Time: 2019-10-11 10:07:18
+'''
 
 # A python script to do a random walker
 # Author: G.H.Zuo
 
-from sys import *
-from numpy import *
+import sys
+import numpy as np
 
 # Basic setup
-NDIM  = 2
+NDIM = 2
 nStep = 100
-if(len(argv)>1) :
-    nStep = int(argv[1])
+if (len(sys.argv) > 1):
+    nStep = int(sys.argv[1])
 
 # Initial state
-r = array([0.0]*NDIM)
-print 0, " ".join(str(c) for c in r)
+r = np.array([0.0] * NDIM)
+print(0, " ".join("{:6.3f}".format(c) for c in r))
 
 # walk around...
-for i in range(1,nStep+1):
-    step = random.sample(NDIM)-0.5
-    step /= linalg.norm(step)
+for i in range(1, nStep + 1):
+    step = np.random.random_sample(NDIM) - 0.5
+    step /= np.linalg.norm(step)
     r += step
-    print i, " ".join(str(c) for c in r)
+    print(i, " ".join("{:6.3f}".format(c) for c in r))
