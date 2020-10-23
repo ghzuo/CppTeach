6d2a77e15f6d9fdf68222bda44e3f29ef3da3d8f
Guanghong Zuo <ghzuo@fudan.edu.cn>
2020-10-23 20:14:23 +0800

    update for debug

--- src/demo/05/read.cpp
+++ src/demo/05/read.cpp
@@ -1,14 +1,15 @@
-#include <string>
 #include <fstream>
 #include <iostream>
+#include <string>
 using namespace std;
 
-int main(){
-    ifstream fp("list.txt");
-    for(string line; getline(fp, line); ){
-        line = line.substr(0,line.find_first_of("#"));
-        if( !line.empty())
-            cout << line << endl;
-    }
-    fp.close();
+int main() {
+  /* read the list file, please careful check the # in the list file */
+  ifstream fp("list.txt");
+  for (string line; getline(fp, line);) {
+    line = line.substr(0, line.find_first_of("#"));
+    if (!line.empty())
+      cout << line << endl;
+  }
+  fp.close();
 }
