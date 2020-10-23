1b3f6c214332af164c892e242c1f8460b995b405
Guangong Zuo <ghzuo@fudan.edu.cn>
2020-10-23 19:13:21 +0800

    update

--- src/demo/05/pointOut.c
+++ src/demo/05/pointOut.c
@@ -7,9 +7,6 @@ int main(int argc, char* argv[]){
     int index;
     int limit = atoi(argv[1]);
 
-    printf("Enter limit (integer: ) ");
-    scanf("%d", &limit);
-
     for(index=0; index < limit; index++){
         array[index] = 0;
         printf("array[%d] is set to 0\n", index);
