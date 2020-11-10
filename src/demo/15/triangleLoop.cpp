5979f9884b08cebcfc132565dab3a2a3732520b1
Guanghong Zuo <ghzuo@fudan.edu.cn>
2020-11-10 21:52:18 +0800

    add arg input for triangle loop testing

--- src/demo/15/triangleLoop.cpp
+++ src/demo/15/triangleLoop.cpp
@@ -13,8 +13,11 @@
 #include <iostream>
 using namespace std;
 
-int main() {
+int main(int argc, char *argv[]) {
   int n = 6;
+  if(argc > 1){
+    n = atoi(argv[1]);
+  }
   int half = n / 2;
 
   for (int i = 1; i <= half; ++i) {
